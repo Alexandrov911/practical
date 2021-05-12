2ebb95cba0bb380277f750cd09044f7054f46fbe
Alexandrov911 <sashok17062002@mail.ru>
2021-05-12 12:29:52 +0300

    Практическая 1

---
+++ Практическая 12.05/2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //11
+            Console.WriteLine("введите число a");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите число b");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите число m");
+            double m = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите число h");
+            double h = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите число p");
+            double p = Convert.ToDouble(Console.ReadLine());
+            double s = ((a+b)/2)*h;
+            double l = m / (p * s);
+            Console.WriteLine(" Толщина доски " +l);
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
---
+++ Практическая 12.05/Практическая 12.05/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая_12._05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            Console.WriteLine("введите число a");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите число r");
+            double r = Convert.ToDouble(Console.ReadLine());
+            r = a / 2;
+            double v = a * a * a;
+            double v1 = (4 / 3) * 3.14 * r * r * r;
+            double m = v - v1;
+            Console.WriteLine(" Объём куба " + v+ " Объём шара " + v1+ " Процент ушедшего материала в отходы "+m);
+            Console.ReadKey();
+
+
+        }
+    }
+}
